67b2ec2e94255ecb8db7cd8c4a0939ac120dbb82
Adrian Likins <alikins@redhat.com>
2014-07-22 10:29:58 -0400

    paramaterize rebuild.sh

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -2,12 +2,23 @@
 #
 # Rebuilds all docker containers from base on down, and pushes each to the internal registry.
 
-cd base
-docker build -t candlepin-base .
-docker tag candlepin-base docker.usersys.redhat.com/dgoodwin/candlepin-base
-docker push docker.usersys.redhat.com/dgoodwin/candlepin-base
+REGISTRY="docker.usersys.redhat.com"
+REGISTRY_USER="${REGISTRY_USER:-alikins}"
 
-cd ../postgresql
-docker build -t candlepin-postgresql .
-docker tag candlepin-postgresql docker.usersys.redhat.com/dgoodwin/candlepin-postgresql
-docker push docker.usersys.redhat.com/dgoodwin/candlepin-postgresql
+
+build_and_push () {
+    TAG="$1"
+    DIR=$2
+    pushd $DIR
+    IMAGE_URI="${REGISTRY}/${REGISTRY_USER}/${TAG}"
+    echo docker build -t "${TAG}" .
+    echo docker tag "${TAG}" "${IMAGE_URI}"
+    echo docker push "${IMAGE_URI}"
+    popd
+}
+
+build_and_push "candlepin-base" base
+build_and_push "candlepin-postgresql" postgresql
+#docker build -t candlepin-postgresql .
+#docker tag candlepin-postgresql docker.usersys.redhat.com/dgoodwin/candlepin-postgresql
+#docker push docker.usersys.redhat.com/dgoodwin/candlepin-postgresql
